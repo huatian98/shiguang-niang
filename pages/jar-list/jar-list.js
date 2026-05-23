c747e95e0f329822dd665b34a8fa3bd53e5c0953
missliu <guohualiu98@163.com>
2026-05-23 14:59:26 +0800

    feat(jar-list): 写死四款产品数据，统一用实物图，展示系列/规格/年份/价格

--- pages/jar-list/jar-list.js
+++ pages/jar-list/jar-list.js
@@ -1,55 +1,20 @@
 const app = getApp()
-const api = require('../../utils/api')
 
-const WEIGHT_MAP = { 1: '十斤', 2: '五斤', 3: '廿斤', 4: '十斤', 5: '五斤' }
-const VILLAGE_MAP = {
-  1: '四坪村', 2: '北墘村', 3: '山头村', 4: '双溪村', 5: '棠口村'
-}
+const STATIC_LIST = [
+  { id: 1, code: 'SP-0001', series_name: '原酿', cellar_village: '四坪村', weight: '10斤', years: '5年', price: 298 },
+  { id: 2, code: 'SP-0002', series_name: '酒酿酒', cellar_village: '北墘村', weight: '5斤', years: '5年', price: 198 },
+  { id: 3, code: 'SP-0003', series_name: '原酿', cellar_village: '山头村', weight: '10斤', years: '1年', price: 198 },
+  { id: 4, code: 'SP-0004', series_name: '酒酿酒', cellar_village: '双溪村', weight: '5斤', years: '1年', price: 118 }
+]
 
 Page({
   data: {
     statusBarHeight: 20,
-    list: [],
-    loading: false
+    list: STATIC_LIST.map(j => ({ ...j, imgSrc: '/images/jars/jar-main.png' }))
   },
 
   onLoad() {
     this.setData({ statusBarHeight: app.globalData.statusBarHeight || 20 })
-  },
-
-  onShow() {
-    this.loadAvailable()
-  },
-
-  onPullDownRefresh() {
-    this.loadAvailable()
-  },
-
-  async loadAvailable() {
-    if (this.data.loading) return
-    this.setData({ loading: true })
-    try {
-      const list = await api.jarsAvailable(50)
-      const formatted = (Array.isArray(list) ? list : []).map(j => {
-        const thumbIdx = ((Number(j.id) - 1) % 3) + 1
-        return {
-          id: j.id,
-          code: j.code,
-          series_name: j.series_name || '惊蛰系列',
-          cellar_village: j.cellar_village || VILLAGE_MAP[Number(j.id) % 5 + 1] || '屏南县',
-          weight: WEIGHT_MAP[Number(j.id) % 5 + 1] || '十斤',
-          price: Number(j.base_price) || 1299,
-          imgSrc: `/images/jars/jar-${thumbIdx}.png`
-        }
-      })
-      this.setData({ list: formatted })
-    } catch (e) {
-      console.error('load available fail', e)
-      this.setData({ list: [] })
-    } finally {
-      this.setData({ loading: false })
-      wx.stopPullDownRefresh()
-    }
   },
 
   onDetail(e) {
